b9e66811120c07eb4bd0e35f274a0b21d42e7704
Samarth Prajapati <102741553+Sam19004@users.noreply.github.com>
2022-09-20 18:12:35 +0530

    Update Another.java

--- Another.java
+++ Another.java
@@ -1,6 +1,8 @@
 public class Another {
     public static void main(String[] args) {
         System.out.println("Another one ");
+        System.out.println("edited by Sam19004 on 20/09/2022 6:11PM ");
+        System.out.println("So keep your projects private that matters on you"); 
     }
     
 }
